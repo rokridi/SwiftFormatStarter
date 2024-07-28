cda5858fbf867f870eff87787b80a8ed48b6f014
mohamed.landolsi <mohamed.landolsi@adevinta.com>
2024-07-28 19:44:08 +0200

    Project setup

---
+++ SwiftFormatStarter/SwiftFormatStarterTests/SwiftFormatStarterTests.swift
@@ -0,0 +1,15 @@
+//
+//  SwiftFormatStarterTests.swift
+//  SwiftFormatStarterTests
+//
+//  Created by Rokridi on 28/07/2024.
+//
+
+@testable import SwiftFormatStarter
+import Testing
+
+struct SwiftFormatStarterTests {
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+}
