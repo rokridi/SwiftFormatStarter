b3c88502dc8a8f6c9e1f9ed0ef7082b6c230e014
mohamed.landolsi <mohamed.landolsi@adevinta.com>
2024-07-28 19:44:08 +0200

    Project setup

---
+++ SwiftFormatStarter/SwiftFormatStarter/SomeStruct.swift
@@ -0,0 +1,18 @@
+//
+//  SomeStruct.swift
+//  SwiftFormatStarter
+//
+//  Created by Rokridi on 28/07/2024.
+//
+
+import Foundation
+
+struct SomeStruct {
+    func foo() {
+        self.bar()
+    }
+
+    func bar() {
+        self.foo()
+    }
+}
---
+++ SwiftFormatStarter/SwiftFormatStarter/SwiftFormatStarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFormatStarterApp.swift
+//  SwiftFormatStarter
+//
+//  Created by Rokridi on 28/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFormatStarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
